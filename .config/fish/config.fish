49e850874e8c4f5a66b8ed95d8bb50c51fe9e452
Leonardo <51390984+leonardoagodoy@users.noreply.github.com>
2025-09-27 22:42:37 -0300

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,183 @@
+﻿## Source from conf.d before our fish config
+source /usr/share/cachyos-fish-config/conf.d/done.fish
+
+
+## Set values
+## Run fastfetch as welcome message
+function fish_greeting
+	##fastfetch
+end
+
+# Format man pages
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# Set settings for https://github.com/franciscolourenco/done
+set -U __done_min_cmd_duration 10000
+set -U __done_notification_urgency_level low
+
+## Environment setup
+# Apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+
+# Add depot_tools to PATH
+if test -d ~/Applications/depot_tools
+    if not contains -- ~/Applications/depot_tools $PATH
+        set -p PATH ~/Applications/depot_tools
+    end
+end
+
+
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Fish command history
+function history
+    builtin history --show-time='%F %T '
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Copy DIR1 DIR2
+function copy
+    set count (count $argv | tr -d \n)
+    if test "$count" = 2; and test -d "$argv[1]"
+        set from (echo $argv[1] | trim-right /)
+        set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
+end
+
+function fish_prompt
+    # Se estiver em venv, mostra o nome dele como prefixo
+    if set -q VIRTUAL_ENV
+        set venv_name (basename "$VIRTUAL_ENV")
+        set_color '#A7F432' # verde claro para destacar
+        echo -n "($venv_name) "
+        set_color normal
+    end
+
+    # Pega o diretório atual e substitui /home/usuario -> ~
+    set cwd (pwd | string replace -r "^$HOME" "~")
+
+    # Diretório com fundo cinza escuro e texto azul claro
+    set_color -b '#252525' '#33B1FF'
+    echo -n " $cwd "
+    set_color normal
+
+    # Icone ❯ em rosa
+    set_color '#EE5396'
+    echo -n " ❯ "
+    set_color normal
+end
+
+
+
+
+## Useful aliases
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+
+# Common use
+alias grubup='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias fixpacman='sudo rm /var/lib/pacman/db.lck'
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
+alias update='sudo pacman -Syu'
+alias zed='zeditor'
+
+# Get fastest mirrors
+alias mirror='sudo cachyos-rate-mirrors'
+
+# Help people new to Arch
+alias please='sudo'
+alias tb='nc termbin.com 9999'
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl='journalctl -p 3 -xb'
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+# Create a localhost server using python3
+alias temp-server='/home/meki/scripts/temp-server.sh'
+
+# Auto deploy may bin to VPS
+alias depo='/home/meki/scripts/deploy.sh'
+
+# Connect via ssh to vps
+alias vps='ssh vps'
+
+zoxide init fish | source
+
+# Yazi wrapper to auto CD into the current direcotory
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
